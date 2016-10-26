c0cb9d0c9516538beadd96eb88d97d07a7de99cd
Jules Dommartin <dommartj@f216-14.e-im2ag.ujf-grenoble.fr>
2016-10-26 12:49:26 +0200

    TP8

---
+++ TP8/src/miniFacebook/DateNaissance.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miniFacebook;
+
+/**
+ *
+ * @author dommartj
+ */
+public class DateNaissance {
+    
+    private int jour;
+    private int mois;
+    private int annee;
+    
+    public DateNaissance() {
+        this.jour = 23;
+        this.mois = 6;
+        this.annee = 1912;
+    }
+
+    public DateNaissance(int jour, int mois, int annee) {
+        this.jour = jour;
+        this.mois = mois;
+        this.annee = annee;
+    }
+
+    public int getJour() {
+        return jour;
+    }
+
+    public void setJour(int jour) {
+        boolean bissextile = ((this.annee % 4 == 0 && this.annee % 100 != 0) || (this.annee % 4 == 0 && this.annee % 100 == 0 && this.annee % 400 == 0));
+        switch (this.mois) {
+            case 1  :
+                if (jour >= 1 && jour <= 31) {
+                    this.jour = jour;
+                } else {
+                    System.out.println("Impossible de modifier le jour.");
+                }   
+            case 2  :
+                if ((bissextile && (jour >= 1 && jour <= 29)) || (!bissextile && (jour >= 1 && jour <= 28))) {
+                    this.jour = jour;
+                } else {
+                    System.out.println("Impossible de modifier le jour.");
+                }   
+            case 3  :
+                if (jour >= 1 && jour <= 31) {
+                    this.jour = jour;
+                } else {
+                    System.out.println("Impossible de modifier le jour.");
+                }   
+            case 4  :
+                if (jour >= 1 && jour <= 30) {
+                    this.jour = jour;
+                } else {
+                    System.out.println("Impossible de modifier le jour.");
+                }   
+            case 5  :
+                if (jour >= 1 && jour <= 31) {
+                    this.jour = jour;
+                } else {
+                    System.out.println("Impossible de modifier le jour.");
+                }   
+            case 6  :
+                if (jour >= 1 && jour <= 30) {
+                    this.jour = jour;
+                } else {
+                    System.out.println("Impossible de modifier le jour.");
+                }   
+            case 7  :
+                if (jour >= 1 && jour <= 31) {
+                    this.jour = jour;
+                } else {
+                    System.out.println("Impossible de modifier le jour.");
+                }   
+            case 8  :
+                if (jour >= 1 && jour <= 31) {
+                    this.jour = jour;
+                } else {
+                    System.out.println("Impossible de modifier le jour.");
+                }   
+            case 9  :
+                if (jour >= 1 && jour <= 30) {
+                    this.jour = jour;
+                } else {
+                    System.out.println("Impossible de modifier le jour.");
+                }   
+            case 10 :
+                if (jour >= 1 && jour <= 31) {
+                    this.jour = jour;
+                } else {
+                    System.out.println("Impossible de modifier le jour.");
+                }   
+            case 11 :
+                if (jour >= 1 && jour <= 30) {
+                    this.jour = jour;
+                } else {
+                    System.out.println("Impossible de modifier le jour.");
+                }   
+            case 12 :
+                if (jour >= 1 && jour <= 31) {
+                    this.jour = jour;
+                } else {
+                    System.out.println("Impossible de modifier le jour.");
+                }             
+            default:
+                System.out.println("Impossible de modifier le jour.");
+                break;
+        }
+        this.jour = jour;
+    }
+
+    public int getMois() {
+        return mois;
+    }
+
+    public void setMois(int mois) {
+        if (mois >= 1 && mois <= 12) {
+            this.mois = mois;
+        } else {
+            System.out.println("Impossible de modifier le mois.");
+        }
+    }
+
+    public int getAnnee() {
+        return annee;
+    }
+
+    public void setAnnee(int annee) {
+        if (annee <= this.annee || annee <= 2016) {
+            this.annee = annee;
+        } else {
+            System.out.println("Impossible de modifier l'année.");
+        }
+    }
+    
+    public void ecritDate() {
+        String s = Integer.toString(this.jour) + "/" + Integer.toString(this.mois) + "/" + Integer.toString(this.annee);
+        System.out.print(s);
+    }
+    
+    // L'annee de référence chez nous sera 2016 (et non pas 2015)
+    public int age() {
+        return 2016 - this.annee;
+    }
+    
+}
